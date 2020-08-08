fdbded26f63c8d99719ba6b7db4e1d64974bb4b2
luis-antonio-ac <luis.developer.ac@gmail.com>
2020-08-07 20:05:52 -0500

    Criacao do componente TextArea

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -5,6 +5,7 @@ import './styles.css'
 import warningIcon  from '../../assets/images/icons/warning.svg'
 
 import { Input } from '../../components/Input';
+import { TextArea } from '../../components/TextArea';
 
 function TeacherForm() {
     return (
@@ -21,6 +22,7 @@ function TeacherForm() {
                     <Input name="name" label="Nome completo"/>
                     <Input name="avatar" label="Avatar"/>
                     <Input name="whatsapp" label="Whatsapp"/>
+                    <TextArea name="Bio" label="Biografia"/>
                 </fieldset>
 
                 <fieldset>
